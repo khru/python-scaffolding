e07f0758c62f38d8a6b45c3f16eac7b31d54e16d
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 15:19:07 +0200

    Test wrap arround

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -10,6 +10,7 @@ class TestKata(unittest.TestCase):
         ["M", "0:1:N", ],
         ["MM", "0:2:N", ],
         ["MMM", "0:3:N", ],
+        ["MMMMMMMMMM", "0:0:N", ],
     ])
     def test_given_a_mars_rover_when_executing_a_move_command_should_move(self, command, expected):
         rover = MarsRover()
