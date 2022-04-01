9a4b81bee55d8cd5a767f43cb9e4ffeee0885fc9
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 15:27:51 +0200

    Implement basic rotate right

--- mars_rover.py
+++ mars_rover.py
@@ -2,4 +2,6 @@ class MarsRover:
     __PLATEAU_SIZE = 10
 
     def execute(self, commands):
+        if commands == 'R':
+            return '0:0:E'
         return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
