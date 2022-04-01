0cca9252262b1bd9b39a0c802e2d0a64349a0e98
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-01 16:54:42 +0200

    mixing movment with rotation to work on the rows

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -61,6 +61,7 @@ class TestKata(unittest.TestCase):
         ["MMR", "0:2:E", ],
         ["MMLL", "0:2:S", ],
         ["MMMMMMMMMMLL", "0:0:S", ],
+        ["RM", "1:0:E", ],
     ])
     def test_given_a_mars_rover_when_executing_a_mixed_rotate_and_move_command_should_rotate_and_move(self, mixed_rotate_move_command, expected):
         rover = MarsRover()
