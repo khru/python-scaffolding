62b96849c4a17c9bde2841b8c559da54f15fb22c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-01 14:40:54 +0200

    test_fresh_mars_rover_does_not_move_on_empty_command passing

---
+++ mars_rover.py
@@ -0,0 +1,3 @@
+class MarsRover:
+    def execute(self, commands):
+        return '0:0:N'
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -1,12 +1,13 @@
 import unittest
+from mars_rover import MarsRover
 
 
 class TestKata(unittest.TestCase):
-    
+
     def test_fresh_mars_rover_does_not_move_on_empty_command(self):
         rover = MarsRover()
-        state = rover.execute()
-        self.assertEquals('0:0:N', state)
+        state = rover.execute('')
+        self.assertEqual('0:0:N', state)
 
 
 if __name__ == '__main__':
