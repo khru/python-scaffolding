5dadb5f85c74125a3f47f02106df9a24d7b161a0
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 16:33:01 +0200

    Implement mix rotation commands

--- mars_rover.py
+++ mars_rover.py
@@ -1,10 +1,31 @@
+class Compass:
+    __compass = ['N', 'E', 'S', 'W']
+ 
+    def __init__(self):
+        self.__orientation = 0
+
+    def rotate_right(self):
+        self.__orientation = (self.__orientation + 1) % len(self.__compass)
+
+    def rotate_left(self):
+        self.__orientation = (self.__orientation - 1) % len(self.__compass)
+
+    def orientation(self):
+        return self.__compass[self.__orientation]
+    
+
 class MarsRover:
     __PLATEAU_SIZE = 10
-    __compass = ['N', 'E', 'S', 'W']
+
+    def __init__(self):
+        self.__compass = Compass()
 
     def execute(self, commands: str):
-        if commands.startswith('L'):
-            return '0:0:' + self.__compass[-len(commands) % len(self.__compass)]
-        if commands.startswith('R'):
-            return '0:0:' + self.__compass[len(commands) % len(self.__compass)]
-        return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
+        if commands.startswith('M'):
+            return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
+        for command in commands:
+            if command == 'L':
+                self.__compass.rotate_left()
+            if command == 'R':
+                self.__compass.rotate_right()
+        return '0:0:' + self.__compass.orientation()
