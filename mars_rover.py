e114264b46940a68f01f2ae4ca76b7e1f2a33a7b
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-01 15:50:16 +0200

    implementing full rotation to the right test_given_a_mars_rover_when_executing_a_rotate_command_should_rotate

--- mars_rover.py
+++ mars_rover.py
@@ -1,7 +1,8 @@
 class MarsRover:
     __PLATEAU_SIZE = 10
     __compass = ['N', 'E', 'S', 'W']
+
     def execute(self, commands: str):
         if commands.startswith('R'):
-            return '0:0:' + self.__compass[len(commands)]
+            return '0:0:' + self.__compass[len(commands) % len(self.__compass)]
         return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
