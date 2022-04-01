f8d1896cca64f08fd04e9bc6956f5678e1a28ce7
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 15:46:20 +0200

    Implement compass

--- mars_rover.py
+++ mars_rover.py
@@ -1,9 +1,7 @@
 class MarsRover:
     __PLATEAU_SIZE = 10
-
-    def execute(self, commands):
-        if commands == 'R':
-            return '0:0:E'
-        if commands == 'RR':
-            return '0:0:S'
+    __compass = ['N', 'E', 'S', 'W']
+    def execute(self, commands: str):
+        if commands.startswith('R'):
+            return '0:0:' + self.__compass[len(commands)]
         return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
