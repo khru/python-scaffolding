06c636a481c8fc71598611638d12f33b4d0deebb
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 15:17:56 +0200

    Implement move through the whole row

--- mars_rover.py
+++ mars_rover.py
@@ -1,7 +1,3 @@
 class MarsRover:
     def execute(self, commands):
-        if commands == 'MM':
-            return '0:2:N'
-        if commands == 'M':
-            return '0:1:N'
-        return '0:0:N'
+        return '0:' + str(len(commands)) + ':N'
