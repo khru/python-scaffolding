80a7c27fff9d1363cc7b4f8da3e70ff9bfafe0a5
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-04-01 16:09:54 +0200

    Implement rotate 3 times

--- mars_rover.py
+++ mars_rover.py
@@ -3,10 +3,8 @@ class MarsRover:
     __compass = ['N', 'E', 'S', 'W']
 
     def execute(self, commands: str):
-        if commands == 'L':
-            return '0:0:W'
-        if commands == 'LL':
-            return '0:0:S'
+        if commands.startswith('L'):
+            return '0:0:' + self.__compass[-len(commands)]
         if commands.startswith('R'):
             return '0:0:' + self.__compass[len(commands) % len(self.__compass)]
         return '0:' + str(len(commands) % self.__PLATEAU_SIZE) + ':N'
