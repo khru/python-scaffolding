a40e2d8054ee40b246c19b6f2515376923ab456b
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-01 15:10:35 +0200

    test_given_a_mars_rover_when_executing_a_move_command_should_move given MMM failing

--- mars_rover.py
+++ mars_rover.py
@@ -1,5 +1,7 @@
 class MarsRover:
     def execute(self, commands):
+        if commands == 'MM':
+            return '0:2:N'
         if commands == 'M':
             return '0:1:N'
         return '0:0:N'
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -1,23 +1,20 @@
 import unittest
+
+from parameterized import parameterized
 from mars_rover import MarsRover
 
 
 class TestKata(unittest.TestCase):
-
-    def test_fresh_mars_rover_does_not_move_on_empty_command(self):
+    @parameterized.expand([
+        ["", "0:0:N", ],
+        ["M", "0:1:N", ],
+        ["MM", "0:2:N", ],
+        ["MMM", "0:3:N", ],
+    ])
+    def test_given_a_mars_rover_when_executing_a_move_command_should_move(self, command, expected):
         rover = MarsRover()
-        state = rover.execute('')
-        self.assertEqual('0:0:N', state)
-
-    def test_given_a_command_move_the_rover_should_move_to_0_1_N(self):
-        rover = MarsRover()
-        state = rover.execute('M')
-        self.assertEqual('0:1:N', state)
-
-    def test_given_a_mars_rover_when_executing_MM_command_should_move_to_0_2_N(self):
-        rover = MarsRover()
-        state = rover.execute('MM')
-        self.assertEqual('0:2:N', state)
+        state = rover.execute(command)
+        self.assertEqual(expected, state)
 
 if __name__ == '__main__':
     unittest.main()
