1e7b878a04d42689f142a3e358536708081c6068
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-04-04 19:21:06 +0200

    making the plateau bigger

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -73,6 +73,16 @@ class TestKata(unittest.TestCase):
         self.assertTrue(unknown_command in str(context.exception))
 
 
+    @parameterized.expand([
+        ["MMMMMMMMMMMMMMMMMMM", "0:19:N", ],
+        ["MMMMMMMMMMMMMMMMMMMM", "0:0:N", ],
+        ["RMMMMMMMMMMMMMMMMMMM", "19:0:E", ],
+        ["RMMMMMMMMMMMMMMMMMMMM", "0:0:E", ],
+    ])
+    def test_given_a_mars_rover_with_a_20_x_20_plateau_when_executing_a_mixed_rotate_command_should_rotate(self, long_command, expected):
+        rover = MarsRover(20)
+        state = rover.execute(long_command)
+        self.assertEqual(expected, state)
 
 if __name__ == '__main__':
     unittest.main()
